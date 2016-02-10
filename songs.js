05276fa524b137983d6e964f29936d989664d754
Thomas Buida <thomasbuida@gmail.com>
2016-02-10 10:22:29 -0600

    Display songs on page using one json file

--- songs.js
+++ songs.js
@@ -1,26 +1,61 @@
-var songs = [];
+// var songs = [];
 
-songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
-songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
-songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
-songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+// songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
+// songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
+// songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
+// songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
+// songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
 
-songs.unshift("Nosetalgia > by Pusha T on the album My Name is My Name");
-songs.push("Juicy > by Notorious BIG on the album Ready to Die");
+// songs.unshift("Nosetalgia > by Pusha T on the album My Name is My Name");
+// songs.push("Juicy > by Notorious BIG on the album Ready to Die");
 
-console.log("songs", songs);
+// console.log("songs", songs);
 
-var songHTML = "";
+// var songHTML = "";
 
-for (var i = 0; i < songs.length; i++){
-  var currentSong = songs[i];
-  currentSong = currentSong.replace(/>/g, "-");
-  currentSong = currentSong.replace(/[^a-zA-Z\-\d\s]/g, "");
-  currentSong = currentSong.split("-");
-  songHTML += '<div class="song"><header>' + currentSong[0] +'</header>';
-  songHTML += '<p>' + currentSong[1] + '</p>';
+// for (var i = 0; i < songs.length; i++){
+//   var currentSong = songs[i];
+//   currentSong = currentSong.replace(/>/g, "-");
+//   currentSong = currentSong.replace(/[^a-zA-Z\-\d\s]/g, "");
+//   currentSong = currentSong.split("-");
+//   songHTML += '<div class="song"><header>' + currentSong[0] +'</header>';
+//   songHTML += '<p>' + currentSong[1] + '</p>';
+// }
+
+// console.log("songs", songs);
+// document.getElementById('songs').innerHTML = songHTML; 
+
+var songsEl = document.getElementById('songs');
+
+var songsRequest = new XMLHttpRequest();
+songsRequest.addEventListener("load", populatePage);
+songsRequest.open("GET", "songs.json");
+songsRequest.send();
+
+
+var title;
+var artist;
+var album;
+
+var songHTML;
+
+function populatePage(){
+  var songsData = JSON.parse(songsRequest.responseText).songs;
+  console.log("songsData", songsData);
+
+  songsData.forEach(function(currentSong){
+    songHTML = "";
+
+    title = currentSong.title;
+    artist = currentSong.artist;
+    album = currentSong.album;
+
+    songHTML += `<div class="song"><header>${title}</header>`;
+    songHTML += `<p>by ${artist} on the album ${album}</p>`;
+
+    songsEl.innerHTML += songHTML;
+  });
+
+
 }
 
-console.log("songs", songs);
-document.getElementById('songs').innerHTML = songHTML; 
